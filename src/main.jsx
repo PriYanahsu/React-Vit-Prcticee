0613f29c3a7aac5da44358511606a4a53bef7038
Priyanshu kumar <priyanshu12656@gmail.com>
2024-09-01 11:45:39 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Priya from './Pkkk.jsx'
 
@@ -9,15 +9,22 @@ function Mypp(){
   return(
     <>
     <h1>click me ! {fun}</h1>
+      {value}
     </>
   )
 }
-createRoot(document.getElementById('root')).render(
+const value = React.createElement(
+  'a',
+  {href = 'https://google.com'},
+  'click Me !'
+  )
+
+const container = document.querrySeletor("#root");
+const root = ReactDOM.createRoot(container);
+root.render(
   <>
-  <StrictMode>
     <Priya />
     <Mypp />
     <App />
-  </StrictMode>,
   </>
 )
